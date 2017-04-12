303f7109be4725b7b891a2b875b1d909768ecb7b
PetrovPetar <petrov90stz@gmail.com>
2017-04-12 19:39:36 +0300

    first commit from Petar to test

--- Avalon.Models/User.cs
+++ Avalon.Models/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,10 +9,13 @@ namespace Avalon.Models
 {
     public class User
     {
+        [Key]
         public int Id { get; set; }
 
+        [Required]
         public string Username { get; set; }
 
+        [Required]
         public string Password { get; set; }
     }
 }
