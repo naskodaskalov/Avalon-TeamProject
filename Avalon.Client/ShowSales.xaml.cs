818a62f2ce532a3e2636319e357e60595fb89cae
killemil <esend3@yahoo.com>
2017-04-18 15:11:35 +0300

    ExportSale

--- Avalon.Client/ShowSales.xaml.cs
+++ Avalon.Client/ShowSales.xaml.cs
@@ -15,6 +15,10 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using iTextSharp;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System.IO;
 
 namespace Avalon.Client
 {
@@ -46,6 +50,59 @@ namespace Avalon.Client
             MainWindow mainWindow = new MainWindow();
             this.Close();
             mainWindow.Show();
+        }
+
+        private void PrintButton_Click(object sender, RoutedEventArgs e)
+        {
+            SalesGrid export = this.ListViewSales.SelectedItem as SalesGrid;
+
+            if (export != null)
+            {
+                FileStream fs = new FileStream("..\\..\\ExportPdf" + "\\" + $"SaleId {export.SaleId} PDF Document.pdf", FileMode.Create);
+
+                Document document = new Document(PageSize.SMALL_PAPERBACK);
+                PdfWriter writer = PdfWriter.GetInstance(document, fs);
+                document.Open();
+                document.AddHeader("Date of sale", export.Date.ToString());
+                document.AddAuthor(export.Seller);
+
+                PdfContentByte cb = writer.DirectContent;
+                iTextSharp.text.Image img = iTextSharp.text.Image.GetInstance("..\\..\\Images\\cap.jpg");
+                img.SetAbsolutePosition(180,30);
+                img.ScalePercent(12);
+                cb.AddImage(img);
+
+                Font calibri = new Font(Font.FontFamily.COURIER, 18, Font.ITALIC);
+
+                iTextSharp.text.Paragraph p1 = new iTextSharp.text.Paragraph($"Data of sale: {export.Date.ToString()}",calibri);
+                iTextSharp.text.Paragraph p2 = new iTextSharp.text.Paragraph($"Customer: {export.Customer}", calibri);
+                iTextSharp.text.Paragraph p3 = new iTextSharp.text.Paragraph($"Number of beers: {export.BeersCount}", calibri);
+                iTextSharp.text.Paragraph p4 = new iTextSharp.text.Paragraph($"Total Sale Price: {export.TotalSalePrice}$", calibri);
+                iTextSharp.text.Paragraph p5 = new iTextSharp.text.Paragraph($"Total Bought Price: {export.TotalBoughtPrice}$", calibri);
+                iTextSharp.text.Paragraph p6 = new iTextSharp.text.Paragraph($"Profit: {export.Profit}$", calibri);
+                iTextSharp.text.Paragraph p7 = new iTextSharp.text.Paragraph($"Seller: {export.Seller}", calibri);
+
+                document.Add(p1);
+                document.Add(p2);
+                document.Add(p3);
+                document.Add(p4);
+                document.Add(p5);
+                document.Add(p6);
+                document.Add(p7);
+
+                document.Close();
+                writer.Close();
+                fs.Close();
+
+                this.WarningLabel.Content = $"Sale with id {export.SaleId} succesfully exported.";
+                this.WarningLabel.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                this.WarningLabel.Content = $"You should select sale first!";
+                this.WarningLabel.Visibility = Visibility.Visible;
+            }
+            
         }
     }
 }
