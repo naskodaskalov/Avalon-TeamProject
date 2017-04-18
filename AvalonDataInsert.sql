7b73b4db09c41be346cb0bbad3c119b431a2df20
killemil <esend3@yahoo.com>
2017-04-18 12:13:02 +0300

    new data

--- AvalonDataInsert.sql
+++ AvalonDataInsert.sql
@@ -1,4 +1,4 @@
-USE AvalonDB
+﻿USE AvalonDB
 GO
 
 SET IDENTITY_INSERT Countries ON
@@ -72,6 +72,19 @@ VALUES (1,'Russian River Brewing Co',1),
 SET IDENTITY_INSERT Breweries OFF
 GO
 
+SET IDENTITY_INSERT Distributors ON
+INSERT INTO Distributors (Id,Name,Address,TownId,Phone)
+VALUES(1,'Bira OOD','Han Asparuh 20',9,'+359 884233432'),
+		(2,'Obedinena Kompania AD','Kraipatno shose 20',9,'+359 332332554345'),
+		(3,'Import Export Ltd.','Berliner strase 22',7,'+49 43249873298498')
+SET IDENTITY_INSERT Distributors OFF
+GO
+
+
+INSERT INTO BreweryDistributors (DistributorId,BreweryId)
+VALUES (1,1),(1,2),(1,3),(1,4),(2,5),(2,6),(2,7),(3,8),(3,9),(3,10),(3,11),(3,12)
+GO
+
 SET IDENTITY_INSERT Awards ON
 INSERT INTO Awards (Id,Name,Year)
 VALUES (1,'Best Belgium Beer','2015'),
@@ -79,42 +92,41 @@ VALUES (1,'Best Belgium Beer','2015'),
 SET IDENTITY_INSERT Awards OFF
 GO
 
-
 SET IDENTITY_INSERT Beers ON
-INSERT INTO Beers (Id,Name,SalePrice,Quantity,Rating,StyleId,BreweryId,DistributorPrice)
-VALUES (1,'Pliny the Elder',4.20,28,9,2,1,3.90),
-		(2,'60 Minute Dogfish',5.30,22,9.5,2,2,5),
-		(3,'Bell�s Hopslam Ale',5,12,8.5,2,3,4),
-		(4,'The Abyss',6,13,10,2,5,5.20),
-		(5,'Guiness Foreign Extra',3.20,30,7,3,6,3),
-		(6,'Saison Dupont',4.20,10,9,5,4,3.80),
-		(7,'Cervesia',3.80,10,8,5,4,3.20),
-		(8,'Redor Pils',3.40,11,9,5,4,2.90),
-		(9,'Moinette Blonde',3.50,22,9,5,4,2.80),
-		(10,'Guiness Original',3.40,50,7,1,6,3),
-		(11,'Guiness Dublin Porter',4.40,66,8,6,6,3.80),
-		(12,'Guiness Golden Ale',4.60,28,9,7,6,3.90),
-		(13,'Guiness Nitro',5.30,89,9.9,2,6,4.60),
-		(14,'Guiness Blonde',4.80,99,9.3,8,6,4.30),
-		(15,'Zagorka Retro',2,45,5,1,7,1.50),
-		(16,'Zagorka Maxx',1.90,23,5,9,7,1.60),
-		(17,'Zagorka Special',2.20,43,6,1,7,1.60),
-		(18,'Heineken',3.30,47,7,1,8,2.40),
-		(19,'Kamenitza Svetlo',2,60,6,1,9,1.79),
-		(20,'Kamenitza Dark',2.80,30,6,1,9,1.99),
-		(21,'Kamenitza Fresh',1.90,40,6,13,9,1.40),
-		(22,'Kamenitza Non Alcohol',1.90,30,5,9,9,1.49),
-		(23,'Kamenitza Old',2.30,30,5,1,9,2.20),
-		(24,'Pirinsko Svetlo',2,30,5,1,10,2.15),
-		(25,'Konrad 14',5,30,8,1,11,4.3),
-		(26,'Konrad 11',4.20,22,8,1,11,4),
-		(27,'Konrad Eso Polotmavy',4.40,29,9,1,11,4),
-		(28,'Amstel',3,39,7,1,12,3.20),
-		(29,'Amstel Bock',3.40,34,8,10,12,2.80),
-		(30,'Stolichno Bock',3.40,33,8,10,7,3.50),
-		(31,'Stolichno Weiss',3,23,7,11,7,2.40),
-		(32,'Stolichno Amber',4,44,8,12,7,3.59),
-		(33,'Konrad  12',4.40,38,9,1,11,4)
+INSERT INTO Beers (Id,Name,SalePrice,Quantity,Rating,StyleId,BreweryId,DistributorPrice,DistributorId)
+VALUES (1,'Pliny the Elder',4.20,28,9,2,1,3.90,1),
+		(2,'60 Minute Dogfish',5.30,22,9.5,2,2,5,1),
+		(3,'Bell’s Hopslam Ale',5,12,8.5,2,3,4,1),
+		(4,'The Abyss',6,13,10,2,5,5.20,2),
+		(5,'Guiness Foreign Extra',3.20,30,7,3,6,3,2),
+		(6,'Saison Dupont',4.20,10,9,5,4,3.80,1),
+		(7,'Cervesia',3.80,10,8,5,4,3.20,1),
+		(8,'Redor Pils',3.40,11,9,5,4,2.90,1),
+		(9,'Moinette Blonde',3.50,22,9,5,4,2.80,1),
+		(10,'Guiness Original',3.40,50,7,1,6,3,2),
+		(11,'Guiness Dublin Porter',4.40,66,8,6,6,3.80,2),
+		(12,'Guiness Golden Ale',4.60,28,9,7,6,3.90,2),
+		(13,'Guiness Nitro',5.30,89,9.9,2,6,4.60,2),
+		(14,'Guiness Blonde',4.80,99,9.3,8,6,4.30,2),
+		(15,'Zagorka Retro',2,45,5,1,7,1.50,2),
+		(16,'Zagorka Maxx',1.90,23,5,9,7,1.60,2),
+		(17,'Zagorka Special',2.20,43,6,1,7,1.60,2),
+		(18,'Heineken',3.30,47,7,1,8,2.40,3),
+		(19,'Kamenitza Svetlo',2,60,6,1,9,1.79,3),
+		(20,'Kamenitza Dark',2.80,30,6,1,9,1.99,3),
+		(21,'Kamenitza Fresh',1.90,40,6,13,9,1.40,3),
+		(22,'Kamenitza Non Alcohol',1.90,30,5,9,9,1.49,3),
+		(23,'Kamenitza Old',2.30,30,5,1,9,2.20,3),
+		(24,'Pirinsko Svetlo',2,30,5,1,10,2.15,3),
+		(25,'Konrad 14',5,30,8,1,11,4.3,3),
+		(26,'Konrad 11',4.20,22,8,1,11,4,3),
+		(27,'Konrad Eso Polotmavy',4.40,29,9,1,11,4,3),
+		(28,'Amstel',3,39,7,1,12,3.20,3),
+		(29,'Amstel Bock',3.40,34,8,10,12,2.80,3),
+		(30,'Stolichno Bock',3.40,33,8,10,7,3.50,2),
+		(31,'Stolichno Weiss',3,23,7,11,7,2.40,2),
+		(32,'Stolichno Amber',4,44,8,12,7,3.59,2),
+		(33,'Konrad  12',4.40,38,9,1,11,4,3)
 SET IDENTITY_INSERT Beers OFF
 GO
 
